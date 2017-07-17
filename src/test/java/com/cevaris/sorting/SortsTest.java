4be6e8c0d7829aede635ab8c868006f930c02dc1
Adam Cardenas <cevaris@gmail.com>
2017-07-17 08:12:53 -0600

    added some sort test helpers

--- src/test/java/com/cevaris/sorting/SortsTest.java
+++ src/test/java/com/cevaris/sorting/SortsTest.java
@@ -1,30 +1,44 @@
 package com.cevaris.sorting;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.cevaris.test.utils.TestUtils;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class SortsTest {
 
   private final int TEST_SIZE = 1000;
 
-  private List<Integer> data = TestUtils.fullyRandomList(TEST_SIZE);
-  private List<Integer> expected = TestUtils.sortedList(TEST_SIZE);
+  private List<Integer> empty = Collections.emptyList();
+  private List<Integer> singleton = Collections.singletonList(1);
+
+  private List<Integer> binary = Arrays.asList(1, 0);
+  private List<Integer> expectedBinary = TestUtils.sortedList(2);
+
+  private List<Integer> randomList = TestUtils.fullyRandomList(TEST_SIZE);
+  private List<Integer> expectedRandomList = TestUtils.sortedList(TEST_SIZE);
 
   @Test
   public void testInsertionSort() {
     Sorter<Integer> sorter = new InsertionSort<>();
-    Assert.assertArrayEquals(expected.toArray(), sorter.sort(data).toArray());
+    assertLists(sorter);
   }
 
-  @Ignore
+  @Test
   public void testMergeSort() {
     Sorter<Integer> sorter = new MergeSort<>();
-    Assert.assertArrayEquals(expected.toArray(), sorter.sort(data).toArray());
+    assertLists(sorter);
+  }
+
+  private void assertLists(Sorter<Integer> sorter) {
+    Assert.assertArrayEquals(empty.toArray(), sorter.sort(empty).toArray());
+    Assert.assertArrayEquals(singleton.toArray(), sorter.sort(singleton).toArray());
+    Assert.assertArrayEquals(expectedBinary.toArray(), sorter.sort(binary).toArray());
+    Assert.assertArrayEquals(expectedRandomList.toArray(), sorter.sort(randomList).toArray());
   }
 
 }
