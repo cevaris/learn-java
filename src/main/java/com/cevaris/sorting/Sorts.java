60381a66dc6148344f4cbb55f2e844f41fb49a98
Adam Cardenas <cevaris@gmail.com>
2017-07-17 08:54:42 -0600

    added merge sort

--- src/main/java/com/cevaris/sorting/Sorts.java
+++ src/main/java/com/cevaris/sorting/Sorts.java
@@ -1,5 +1,6 @@
 package com.cevaris.sorting;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,6 +23,37 @@ class InsertionSort<E extends Comparable<E>> implements Sorter<E> {
 class MergeSort<E extends Comparable<E>> implements Sorter<E> {
   @Override
   public List<E> sort(List<E> arr) {
-    return arr;
+    if (arr.size() <= 1) return arr;
+
+    int m = arr.size() / 2;
+    List<E> ls = sort(arr.subList(0, m));
+    List<E> rs = sort(arr.subList(m, arr.size()));
+
+    return merge(ls, rs);
+  }
+
+  private List<E> merge(List<E> ls, List<E> rs) {
+    int msLen = ls.size() + rs.size();
+    List<E> ms = new ArrayList<>(msLen);
+
+    for (int li = 0, ri = 0, mi = 0; mi < msLen; mi++) {
+      if (rs.size() <= ri && ls.size() > li) {
+        ms.add(ls.get(li++));
+        continue;
+      }
+
+      if (ls.size() <= li && rs.size() > ri) {
+        ms.add(rs.get(ri++));
+        continue;
+      }
+
+      if (ls.get(li).compareTo(rs.get(ri)) < 0) {
+        ms.add(ls.get(li++));
+      } else {
+        ms.add(rs.get(ri++));
+      }
+    }
+
+    return ms;
   }
 }
