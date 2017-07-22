3720189b3b6a8e9f2f2d5c5a24c1bc659fd1b487
Adam Cardenas <cevaris@gmail.com>
2017-07-22 16:47:14 -0600

    calcuated fib(100000)

--- src/main/java/com/cevaris/recursion/Fib.java
+++ src/main/java/com/cevaris/recursion/Fib.java
@@ -1,7 +1,11 @@
 package com.cevaris.recursion;
 
+import java.math.BigInteger;
+
 interface Fibonacci {
   long fib(int n);
+
+  BigInteger fibl(int n);
 }
 
 class SimpleRecursiveFib implements Fibonacci {
@@ -12,20 +16,30 @@ class SimpleRecursiveFib implements Fibonacci {
     if (n == 1 || n == 2) return 1;
     return fib(n - 1) + fib(n - 2);
   }
+
+  @Override
+  public BigInteger fibl(int n) {
+    return BigInteger.valueOf(fib(n));
+  }
 }
 
 class MemoizedFib implements Fibonacci {
   @Override
   public long fib(int n) {
-    if (n < 0) throw new IllegalArgumentException();
-    if (n == 0 || n == 1) return 1L;
+    return fibl(n).longValue();
+  }
 
-    long[] m = new long[n + 1];
-    m[0] = 1L;
-    m[1] = 1L;
+  @Override
+  public BigInteger fibl(int n) {
+    if (n < 0) throw new IllegalArgumentException();
+    if (n == 0 || n == 1) return BigInteger.ONE;
+
+    BigInteger[] m = new BigInteger[n + 1];
+    m[0] = BigInteger.ONE;
+    m[1] = BigInteger.ONE;
 
     for (int i = 2; i <= n; i++) {
-      m[i] = m[i - 1] + m[i - 2];
+      m[i] = m[i - 1].add(m[i - 2]);
     }
 
     return m[n - 1];
@@ -48,5 +62,10 @@ class MemoizedRecursiveFib implements Fibonacci {
     }
 
     return m[n];
+  }
+
+  @Override
+  public BigInteger fibl(int n) {
+    return BigInteger.valueOf(fib(n));
   }
 }
